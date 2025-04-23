fab040e2f26181a90efb3fbceb4bffea3312f4ee
Derek Weix <47987393+dpweix@users.noreply.github.com>
2025-04-23 14:37:48 -0400

    Added get_MMT function
    I renamed the get_cen function to get_MMT to more specifically reflect the purpose of the function (getting the value of the exposure that corresponds to the lowest value of the response).

---
+++ R/get_MMT.R
@@ -0,0 +1,42 @@
+#' Get the MMT
+#'
+#' When making predictions with distributed lag nonlinear models, this function
+#' retrieves the value of the exposure that corresponds to the minimum value of
+#' the response. These models are commonly used to predict mortality as a function
+#' of temperature, for which this function returns the minimum mortality temperature
+#' (MMT).
+#'
+#' @param crosspred An object of class "`crosspred`" or "`crossreduce`".
+#'
+#' @returns The numeric value of the exposure corresponding to the minimum value of the response.
+#' @export
+#'
+#' @examples
+#' # The NYC airquality sample dataset
+#' help(airquality)
+#'
+#' # Arguments to specify cross-basis
+#' arg_var <- list(fun = "ns", knots = quantile(airquality$Temp, c(.1, .75, .9)))
+#' arg_lag <- list(fun = "ns", knots = logknots(7, nk = 3))
+#'
+#' # Cross-basis and model fit
+#' cb <- crossbasis(airquality$Temp, lag = 7, argvar = arg_var, arglag = arg_lag)
+#' fit <- glm(Ozone ~ cb, family = "quasipoisson", data = airquality)
+#'
+#' # Prediction with default centering
+#' pred <- crosspred(cb, fit)
+#' plot(pred, "overall")
+#'
+#' # Prediction centering at the MMT
+#' pred_centered <- crosspred(cb, fit, cen = get_MMT(pred))
+#' plot(pred_centered, "overall")
+
+get_MMT <- function(crosspred) {
+  if(!is.null(crosspred$fit)) {
+    crosspred$predvar[which.min(crosspred$fit)]
+  } else if(!is.null(crosspred$allfit)) {
+    crosspred$predvar[which.min(crosspred$allfit)]
+  } else {
+    print("Unable to locate minimum.")
+  }
+}
