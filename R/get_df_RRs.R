92b38391580bf5591aa818b1df66346810a9e26e
Derek Weix <47987393+dpweix@users.noreply.github.com>
2025-04-30 10:57:30 -0400

    Remove var_name argument

--- R/get_df_RRs.R
+++ R/get_df_RRs.R
@@ -6,10 +6,9 @@
 #' associated with the RR or output.
 #'
 #' @param pred An object of class "`crossreduce`".
-#' @param var_name The name of the exposure variable of interest.
 #'
 #' @returns A tibble with with the following columns:
-#'  - `Temperature`: The values of the predictor variable. Column name determined by the `var_name` argument.
+#'  - `exposure`: The values of the predictor variable.
 #'  - `RR`: The predicted risk, rate, or odds ratio from `crossreduce`.
 #'  - `RR_low`, `RR_high`: The lower/upper bound of the confidence bands for `RR`. Depends on the `ci.level` argument in `crossreduce`.
 #'  - `fit`: The predicted output from `crossreduce`.
@@ -18,8 +17,11 @@
 #' @export
 #'
 #' @examples
+#'
+#' library(dlnm)
+#' library(ggplot2)
 #' # The NYC airquality sample dataset
-#' help(airquality)
+#' head(airquality, 10)
 #'
 #' # Arguments to specify cross-basis
 #' arg_var <- list(fun = "ns", knots = quantile(airquality$Temp, c(.1, .75, .9)))
@@ -40,16 +42,16 @@
 #' # Plots with ggplot2
 #' df_RR1 |>
 #'   ggplot() +
-#'   geom_line(aes(Temperature, fit)) +
-#'   geom_ribbon(aes(Temperature, ymin = low, ymax = high), alpha = .2)
+#'   geom_line(aes(exposure, fit)) +
+#'   geom_ribbon(aes(exposure, ymin = low, ymax = high), alpha = .2)
 #'
 #' df_RR2 |>
 #'   ggplot() +
-#'   geom_line(aes(Temperature, RR)) +
-#'   geom_ribbon(aes(Temperature, ymin = RR_low, ymax = RR_high), alpha = .2)
+#'   geom_line(aes(exposure, RR)) +
+#'   geom_ribbon(aes(exposure, ymin = RR_low, ymax = RR_high), alpha = .2)
 
-get_df_RRs <- function(pred, var_name = "Temperature") {
-  tibble::tibble(!!var_name := pred$predvar,
+get_df_RRs <- function(pred) {
+  tibble::tibble(exposure = pred$predvar,
                  RR = pred$RRfit,
                  RR_low = pred$RRlow,
                  RR_high = pred$RRhigh,
